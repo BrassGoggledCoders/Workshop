dc3b09d95443557e205208eba32c382b277e3123
Drizzs <drizzs@hotmail.com>
2020-01-07 17:45:27 -0500

    Obsidian Plate + Test Recipes

---
+++ src/main/java/xyz/brassgoggledcoders/workshop/blocks/ObsidianPlate.java
@@ -0,0 +1,11 @@
+package xyz.brassgoggledcoders.workshop.blocks;
+
+import net.minecraft.block.PressurePlateBlock;
+
+public class ObsidianPlate extends PressurePlateBlock {
+
+    public ObsidianPlate(Sensitivity sensitivity, Properties properties) {
+        super(sensitivity, properties);
+    }
+
+}
