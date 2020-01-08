2fdbaf5073c556a50d333b437ba32a8238ed9564
Drizzs <drizzs@hotmail.com>
2020-01-08 18:51:11 -0500

    Remove Duplicate Entry

--- src/main/java/xyz/brassgoggledcoders/workshop/registries/Items.java
+++ src/main/java/xyz/brassgoggledcoders/workshop/registries/Items.java
@@ -5,7 +5,6 @@ import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
-import xyz.brassgoggledcoders.workshop.util.WorkGroup;
 
 import static xyz.brassgoggledcoders.workshop.Workshop.MOD_ID;
 
@@ -13,11 +12,8 @@ public class Items {
 
     private static final DeferredRegister<Item> ITEMS = new DeferredRegister<>(ForgeRegistries.ITEMS, MOD_ID);
     public static final RegistryObject<Item> WORKICON = ITEMS.register("workicon", () -> new Item(new Item.Properties()));
+
 /*
-    private static final DeferredRegister<Item> ITEMS = new DeferredRegister<>(ForgeRegistries.ITEMS, MOD_ID);
-
-    public static final RegistryObject<Item> WORKICON = ITEMS.register("workicon", () -> new Item(new Item.Properties()));
-
     //Alembic Outputs
     public static final RegistryObject<Item> ADHESIVE_OIL = ITEMS.register("adhesive_oil", () -> new Item(new Item.Properties().group(WorkGroup.instance)));
     public static final RegistryObject<Item> DISTILLED_WATER = ITEMS.register("distilled_water", () -> new Item(new Item.Properties().group(WorkGroup.instance)));
