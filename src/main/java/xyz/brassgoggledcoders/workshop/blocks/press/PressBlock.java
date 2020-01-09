c6d2e65144193b1f77d2885cea4cb8c30c0787a8
Drizzs <drizzs@hotmail.com>
2020-01-09 16:03:40 -0500

    Press Rotation Handling/assets + Barrel assets

--- src/main/java/xyz/brassgoggledcoders/workshop/blocks/press/PressBlock.java
+++ src/main/java/xyz/brassgoggledcoders/workshop/blocks/press/PressBlock.java
@@ -2,17 +2,46 @@ package xyz.brassgoggledcoders.workshop.blocks.press;
 
 
 import com.hrznstudio.titanium.api.IFactory;
+import com.hrznstudio.titanium.block.BlockRotation;
 import com.hrznstudio.titanium.block.BlockTileBase;
+import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
+import net.minecraft.util.BlockRenderLayer;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockReader;
 
-public class PressBlock extends BlockTileBase {
+import javax.annotation.Nonnull;
+
+public class PressBlock extends BlockRotation<PressTile> {
+
 
     public PressBlock() {
         super("press", Properties.from(Blocks.IRON_BLOCK), PressTile.class);
     }
 
+    @Nonnull
+    @Override
+    public RotationType getRotationType() {
+        return RotationType.FOUR_WAY;
+    }
+
     @Override
     public IFactory getTileEntityFactory() {
         return PressTile::new;
+    }
+
+    @Override
+    public boolean isSolid(BlockState state) {
+        return false;
+    }
+
+    @Override
+    public boolean isNormalCube(BlockState state, IBlockReader worldIn, BlockPos pos) {
+        return false;
+    }
+
+    @Override
+    public BlockRenderLayer getRenderLayer() {
+        return BlockRenderLayer.CUTOUT;
     }
 }
