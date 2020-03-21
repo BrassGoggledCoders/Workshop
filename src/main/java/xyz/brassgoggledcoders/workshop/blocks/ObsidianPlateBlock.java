fb9d03de7ac1358b58fd627df026798b568f5174
FreneticScribbler <aj@aronajones.com>
2020-03-21 15:30:18 +0000

    Impl broken anvil
    Though at the minute it doesn't spawn when an anvil breaks

--- src/main/java/xyz/brassgoggledcoders/workshop/blocks/ObsidianPlateBlock.java
+++ src/main/java/xyz/brassgoggledcoders/workshop/blocks/ObsidianPlateBlock.java
@@ -2,9 +2,9 @@ package xyz.brassgoggledcoders.workshop.blocks;
 
 import net.minecraft.block.PressurePlateBlock;
 
-public class ObsidianPlate extends PressurePlateBlock {
+public class ObsidianPlateBlock extends PressurePlateBlock {
 
-    public ObsidianPlate(Sensitivity sensitivity, Properties properties) {
+    public ObsidianPlateBlock(Sensitivity sensitivity, Properties properties) {
         super(sensitivity, properties);
     }
 
--- src/main/java/xyz/brassgoggledcoders/workshop/registries/WorkshopBlocks.java
+++ src/main/java/xyz/brassgoggledcoders/workshop/registries/WorkshopBlocks.java
@@ -11,16 +11,27 @@ import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
-import xyz.brassgoggledcoders.workshop.blocks.ObsidianPlate;
+import xyz.brassgoggledcoders.workshop.Workshop;
+import xyz.brassgoggledcoders.workshop.blocks.BrokenAnvilBlock;
+import xyz.brassgoggledcoders.workshop.blocks.ObsidianPlateBlock;
 
 public class WorkshopBlocks {
 
     private static final DeferredRegister<Block> BLOCKS = new DeferredRegister<>(ForgeRegistries.BLOCKS, MOD_ID);
     private static final DeferredRegister<Item> ITEMS = new DeferredRegister<>(ForgeRegistries.ITEMS, MOD_ID);
 
-    public static final RegistryObject<Block> OBSIDIAN_PLATE = BLOCKS.register("obsidian_plate", () -> new ObsidianPlate(PressurePlateBlock.Sensitivity.EVERYTHING,Block.Properties.create(Material.ROCK)));
+    public static final RegistryObject<Block> OBSIDIAN_PLATE = BLOCKS.register("obsidian_plate",
+            () -> new ObsidianPlateBlock(PressurePlateBlock.Sensitivity.EVERYTHING,
+                    Block.Properties.create(Material.ROCK)));
 
-    public static final RegistryObject<Item> OBSIDIAN_PLATE_ITEM = ITEMS.register("obsidian_plate", () -> new BlockItem(OBSIDIAN_PLATE.get(),new Item.Properties()));
+    public static final RegistryObject<Item> OBSIDIAN_PLATE_ITEM = ITEMS.register("obsidian_plate",
+            () -> new BlockItem(OBSIDIAN_PLATE.get(), new Item.Properties().group(Workshop.workshopTab)));
+
+    public static final RegistryObject<Block> BROKEN_ANVIL = BLOCKS.register("broken_anvil",
+            () -> new BrokenAnvilBlock(Block.Properties.create(Material.ANVIL)));
+
+    public static final RegistryObject<Item> BROKEN_ANVIL_ITEM = ITEMS.register("broken_anvil",
+            () -> new BlockItem(BROKEN_ANVIL.get(), new Item.Properties().group(Workshop.workshopTab)));
 
     public static void register(IEventBus bus) {
         BLOCKS.register(bus);
