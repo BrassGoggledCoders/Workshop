d4db33083ae9b63f6f0a940c34059a89ed37be14
FreneticScribbler <aj@aronajones.com>
2020-03-27 22:35:23 +0000

    Implement spinning wheel model

--- src/main/java/xyz/brassgoggledcoders/workshop/blocks/spinningwheel/SpinningWheelBlock.java
+++ src/main/java/xyz/brassgoggledcoders/workshop/blocks/spinningwheel/SpinningWheelBlock.java
@@ -2,12 +2,25 @@ package xyz.brassgoggledcoders.workshop.blocks.spinningwheel;
 
 import com.hrznstudio.titanium.api.IFactory;
 import com.hrznstudio.titanium.block.BasicTileBlock;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
+import net.minecraft.state.DirectionProperty;
+import net.minecraft.state.StateContainer;
+import net.minecraft.state.properties.BlockStateProperties;
+import net.minecraft.util.Direction;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.shapes.ISelectionContext;
+import net.minecraft.util.math.shapes.VoxelShape;
+import net.minecraft.world.IBlockReader;
 
 public class SpinningWheelBlock extends BasicTileBlock<SpinningWheelTile> {
 
+    public static final DirectionProperty FACING = BlockStateProperties.HORIZONTAL_FACING;
+
     public SpinningWheelBlock() {
-        super(Properties.from(Blocks.IRON_BLOCK), SpinningWheelTile.class);
+        super(Properties.from(Blocks.OAK_PLANKS).notSolid(), SpinningWheelTile.class);
+        this.setDefaultState(this.getDefaultState().with(FACING, Direction.NORTH));
     }
 
     @Override
@@ -15,4 +28,14 @@ public class SpinningWheelBlock extends BasicTileBlock<SpinningWheelTile> {
         return SpinningWheelTile::new;
     }
 
+    @Override
+    public boolean isNormalCube(BlockState state, IBlockReader worldIn, BlockPos pos) {
+        return false;
+    }
+
+    @Override
+    protected void fillStateContainer(StateContainer.Builder<Block, BlockState> builder) {
+        super.fillStateContainer(builder);
+        builder.add(FACING);
+    }
 }
