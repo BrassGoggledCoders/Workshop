27b971fab794350e3cc7f113508657895626290c
FreneticScribbler <aj@aronajones.com>
2020-04-11 16:25:19 +0100

    Fix access transformer

--- src/main/java/xyz/brassgoggledcoders/workshop/api/FurnaceCollectorTarget.java
+++ src/main/java/xyz/brassgoggledcoders/workshop/api/FurnaceCollectorTarget.java
@@ -1,10 +1,7 @@
 package xyz.brassgoggledcoders.workshop.api;
 
 import net.minecraft.item.ItemStack;
-import net.minecraft.item.crafting.IRecipe;
-import net.minecraft.item.crafting.IRecipeType;
 import net.minecraft.tileentity.FurnaceTileEntity;
-import net.minecraft.tileentity.TileEntity;
 import org.apache.commons.lang3.ArrayUtils;
 
 public class FurnaceCollectorTarget implements ICollectorTarget {
