3a9df1a112d56ce8cefd559cac60dc2ce6371d10
Arona <aj@aronajones.com>
2020-06-03 21:29:01 +0100

    'Temporary' Mortar/pestle model

--- src/main/java/xyz/brassgoggledcoders/workshop/block/MortarBlock.java
+++ src/main/java/xyz/brassgoggledcoders/workshop/block/MortarBlock.java
@@ -1,11 +1,31 @@
 package xyz.brassgoggledcoders.workshop.block;
 
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.shapes.ISelectionContext;
+import net.minecraft.util.math.shapes.VoxelShape;
+import net.minecraft.world.IBlockReader;
 import xyz.brassgoggledcoders.workshop.tileentity.MortarTileEntity;
+
+import javax.annotation.Nonnull;
 
 public class MortarBlock extends TileBlock<MortarTileEntity> {
 
     public MortarBlock() {
-        super(Properties.from(Blocks.GRANITE), MortarTileEntity::new);
+        super(Properties.from(Blocks.GRANITE).notSolid(), MortarTileEntity::new);
+    }
+
+    @Override
+    public boolean isNormalCube(BlockState state, IBlockReader worldIn, BlockPos pos) {
+        return false;
+    }
+
+    @Override
+    @Nonnull
+    @SuppressWarnings("deprecation")
+    public VoxelShape getShape(BlockState state, IBlockReader worldIn, BlockPos pos, ISelectionContext context) {
+        return Block.makeCuboidShape(4.0D, 0.0D, 4.0D, 12.0D, 3.0D, 12.0D);
     }
 }
