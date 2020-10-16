4b4c0b26a1e1a317a46a605ab0825b74e44fd090
FreneticScribbler <aj@aronajones.com>
2020-10-16 10:06:52 +0100

    Scrap bags work in dispenser

---
+++ src/main/java/xyz/brassgoggledcoders/workshop/item/DispenseScrapBagBehaviour.java
@@ -0,0 +1,29 @@
+package xyz.brassgoggledcoders.workshop.item;
+
+import net.minecraft.block.DispenserBlock;
+import net.minecraft.dispenser.DefaultDispenseItemBehavior;
+import net.minecraft.dispenser.IBlockSource;
+import net.minecraft.dispenser.IPosition;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.Direction;
+import net.minecraft.world.server.ServerWorld;
+import xyz.brassgoggledcoders.workshop.content.WorkshopItems;
+
+import javax.annotation.Nonnull;
+import javax.annotation.ParametersAreNonnullByDefault;
+import java.util.List;
+
+public class DispenseScrapBagBehaviour extends DefaultDispenseItemBehavior {
+    @Override
+    @Nonnull
+    @ParametersAreNonnullByDefault
+    protected ItemStack dispenseStack(IBlockSource source, ItemStack stack) {
+        if(!source.getWorld().isRemote() && WorkshopItems.SCRAP_BAG.get().equals(stack.getItem())) {
+            List<ItemStack> list = ScrapBagItem.getLoot((ServerWorld) source.getWorld(), null, source.getBlockPos());
+            list.forEach(istack -> super.dispenseStack(source, istack));
+            stack.shrink(1);
+            return stack;
+        }
+        return super.dispenseStack(source,stack);
+    }
+}
--- src/main/java/xyz/brassgoggledcoders/workshop/item/ScrapBagItem.java
+++ src/main/java/xyz/brassgoggledcoders/workshop/item/ScrapBagItem.java
@@ -1,11 +1,13 @@
 package xyz.brassgoggledcoders.workshop.item;
 
+import net.minecraft.block.DispenserBlock;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.potion.EffectInstance;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraft.world.server.ServerWorld;
 import net.minecraft.world.storage.loot.LootContext;
@@ -14,29 +16,47 @@ import net.minecraft.world.storage.loot.LootParameters;
 import net.minecraftforge.items.ItemHandlerHelper;
 import net.minecraftforge.items.wrapper.PlayerInvWrapper;
 import xyz.brassgoggledcoders.workshop.content.WorkshopEffects;
+import xyz.brassgoggledcoders.workshop.content.WorkshopItems;
 import xyz.brassgoggledcoders.workshop.datagen.loot.WorkshopGiftLootTables;
 
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import javax.annotation.ParametersAreNonnullByDefault;
+import java.util.Collections;
 import java.util.List;
 
 public class ScrapBagItem extends Item {
     public ScrapBagItem(Properties properties) {
         super(properties);
+        DispenserBlock.registerDispenseBehavior(this, new DispenseScrapBagBehaviour());
     }
 
+    @Override
+    @ParametersAreNonnullByDefault
+    @Nonnull
     public ActionResult<ItemStack> onItemRightClick(World worldIn, PlayerEntity playerIn, Hand handIn) {
         if (!worldIn.isRemote) {
-            List<ItemStack> list =
-                    worldIn.getServer()
-                            .getLootTableManager()
-                            .getLootTableFromLocation(WorkshopGiftLootTables.SCRAP_BAG)
-                            .generate(new LootContext.Builder((ServerWorld) worldIn)
-                                    .withParameter(LootParameters.POSITION, playerIn.getPosition()).withParameter(LootParameters.THIS_ENTITY, playerIn)
-                                    .build(LootParameterSets.GIFT));
-            list.forEach(stack -> ItemHandlerHelper.insertItem(new PlayerInvWrapper(playerIn.inventory), stack, false));
+            getLoot((ServerWorld) worldIn, playerIn, null).forEach(stack -> ItemHandlerHelper.insertItem(new PlayerInvWrapper(playerIn.inventory), stack, false));
             playerIn.getHeldItem(handIn).shrink(1);
             playerIn.addPotionEffect(new EffectInstance(WorkshopEffects.STINKY.get(), 20 * 60 * 5));
             return ActionResult.resultSuccess(playerIn.getHeldItem(handIn));
         }
         return super.onItemRightClick(worldIn, playerIn, handIn);
+    }
+
+    public static List<ItemStack> getLoot(ServerWorld worldIn, @Nullable PlayerEntity playerIn, @Nullable BlockPos position) {
+        if(position == null && playerIn != null) {
+            position = playerIn.getPosition();
+        }
+        if(position != null) {
+            return worldIn.getServer()
+                    .getLootTableManager()
+                    .getLootTableFromLocation(WorkshopGiftLootTables.SCRAP_BAG)
+                    .generate(new LootContext.Builder(worldIn)
+                            .withParameter(LootParameters.POSITION, position).withNullableParameter(LootParameters.THIS_ENTITY, playerIn)
+                            //Has to have pos, entity is optional
+                            .build(LootParameterSets.COMMAND));
+        }
+        return Collections.emptyList();
     }
 }
