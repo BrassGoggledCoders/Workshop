d34343056bae7eb2f4e9c07aa7c54646e4cc1b6d
Skylar <skylar.som@gmail.com>
2021-12-02 19:50:38 -0500

    Glorious nuking

--- src/main/java/xyz/brassgoggledcoders/workshop/api/capabilities/CollectorTarget.java
+++
@@ -1,9 +0,0 @@
-package xyz.brassgoggledcoders.workshop.api.capabilities;
-
-import net.minecraft.item.ItemStack;
-
-public interface CollectorTarget {
-    ItemStack[] getCollectables();
-
-    boolean isActive();
-}
--- src/main/java/xyz/brassgoggledcoders/workshop/recipe/IMachineRecipe.java
+++
@@ -1,8 +0,0 @@
-package xyz.brassgoggledcoders.workshop.recipe;
-
-import net.minecraft.inventory.IInventory;
-import net.minecraft.item.crafting.IRecipe;
-
-public interface IMachineRecipe extends IRecipe<IInventory> {
-    int getProcessingTime();
-}
